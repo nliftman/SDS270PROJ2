37360dce1a979013ea43859dc2f61e1eade0837e
Naomi Liftman <nliftman@smith.edu>
2022-11-17 10:08:43 -0500

    adding dataset

---
+++ R/primes.R
@@ -0,0 +1,14 @@
+#' Prime Numbers
+#'
+#' The first 1000 prime numbers.
+#'
+#' @format ## `primes`
+#' A data frame with 1,000 rows and 2 columns:
+#' \describe{
+#'   \item{n}{The chronological prime number order}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"who"
