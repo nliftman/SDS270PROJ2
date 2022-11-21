84c4f7c7ca2e88745d7531369853243bc4feae9e
Naomi Liftman <nliftman@smith.edu>
2022-11-21 18:25:27 -0500

    adding tests of area

--- tests/testthat/test-area.R
+++ tests/testthat/test-area.R
@@ -1,7 +1,3 @@
-test_that("find the area of a square correctly", {
-  expect_equal(area_square(2,2), 4)
-})
-
 test_that("find the area of a circle correctly", {
   expect_equal(area_circle(1.1283791670955126), 4)
 })
@@ -25,3 +21,8 @@ test_that("find the area of a hexagon correctly", {
 test_that("find the area of a octogon correctly", {
   expect_equal(area_octagon(3), 43.455844)
 })
+
+test_that("find the area of a square correctly", {
+  expect_equal(area_square(2,2), 4)
+})
+
