6bae61f30200e856f36b1bc25e0bd4f81b7dcfa1
Naomi Liftman <nliftman@smith.edu>
2022-11-29 10:17:14 -0500

    attempting to add graph to origional function and failing :(

--- R/area_square.R
+++ R/area_square.R
@@ -4,22 +4,20 @@
 #'
 #' @param x Width of the square (positive number)
 #' @param y Height of the square (positive number)
+#' @param graph Graphs the shape of the square
 #'
-#' @return The area of a square with width x and height y.
+#' @return The area of a square with width x and height y, and a graph of the square if requested.
+#' @import ggplot2
 #' @export
 #'
 #' @examples
 #' area_square(5,4)
-area_square <- function(x,y, graph){
+area_square <- function(x,y){
   if ((!is.numeric(x)) | (!is.numeric(y)) | x < 0 | y < 0 ){
     stop('Squares must have positive and numeric widths and heights!', call. = FALSE)
   }
   else{
     z <- x*y
     return(z)
-  }
-  if (graph == 'y' | graph == 'Yes' | graph == 'yes' | graph == 'Y'){
-    ggplot() +
-      geom_point(aes(x = x, y = y))
   }
 }
