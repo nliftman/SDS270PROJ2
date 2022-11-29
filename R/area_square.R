b264741a4e98367b6ac8407dfc5804ff6e2587de
Naomi Liftman <nliftman@smith.edu>
2022-11-29 10:10:49 -0500

    adding import ggplot, and building graph of square

--- R/area_square.R
+++ R/area_square.R
@@ -10,12 +10,16 @@
 #'
 #' @examples
 #' area_square(5,4)
-area_square <- function(x,y){
+area_square <- function(x,y, graph){
   if ((!is.numeric(x)) | (!is.numeric(y)) | x < 0 | y < 0 ){
     stop('Squares must have positive and numeric widths and heights!', call. = FALSE)
   }
   else{
     z <- x*y
     return(z)
+  }
+  if (graph == 'y' | graph == 'Yes' | graph == 'yes' | graph == 'Y'){
+    ggplot() +
+      geom_point(aes(x = x, y = y))
   }
 }
