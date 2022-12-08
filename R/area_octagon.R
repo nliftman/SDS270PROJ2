7ec3ce9440c73881be869281371b4022126640b6
Naomi Liftman <nliftman@smith.edu>
2022-12-07 20:58:23 -0500

    move area of circle, because vignette isnt working

--- R/area_octagon.R
+++ R/area_octagon.R
@@ -3,18 +3,28 @@
 #' A simple function to find the area of a regular octagon
 #'
 #' @param x Length of one side of the regular octagon (positive number)
+#' @param graph Indicator variable for graphing option (0 for no graph, 1 for graph)
 #'
 #' @return The area of a regular octagon with side lengths x.
 #' @export
 #'
 #' @examples
-#' area_octagon(3)
-area_octagon <- function(x){
+#' area_octagon(3, 0 )
+area_octagon <- function(x, graph){
   if (!is.numeric(x) | x < 0 ){
     stop("Side lengths of octagons must be positive and numeric!")
   }
   else{
     t <- {(2*(1+sqrt(2)))}*x^2
-    return(t)
+    if (graph == 1){
+      print(shapez::graph_octagon(x))
+      return(t)
+    }
+    else if (graph == 0){
+      return(t)
+    }
+    else{
+      stop('You must input either 0 or 1 to determine graphing options')
+    }
   }
 }
