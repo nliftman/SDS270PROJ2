26d593bad8ce62ce29151fb800911793863c3eb5
Naomi Liftman <nliftman@smith.edu>
2022-12-07 20:50:42 -0500

    fix tests

--- tests/testthat/test-area.R
+++ tests/testthat/test-area.R
@@ -22,17 +22,6 @@ test_that("find the area of a hexagon correctly, without a graph", {
   expect_equal(area_hexagon(3,0), 23.382686)
 })
 
-test_that("find the area of a octogon correctly", {
-  expect_equal(area_octagon(3), 43.455844)
+test_that("find the area of a octogon correctly, without a graph", {
+  expect_equal(area_octagon(3, 0), 43.455844)
 })
-
-fuck() <- function(x){
-  a = ((sin(pi/3)*x)/(sin(pi/2)))
-  octagon_df <- data.frame(x = c(0, a, x+a, 2*a + x, 2*a + x, a+x, a),
-                           y = c(a, 0, 0, a, x+a, 2*a+x, 2*a+x))
-
-  ggplot(data = octagon_df) +
-    geom_point(aes(x,y)) +
-    xlab("Width of Square") +
-    ylab("Height of Square")
-}
