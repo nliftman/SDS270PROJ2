bbb2a02b0946e33fac896ec6b68f0062ed8337e8
nliftman <nliftman@rstudio.smith.edu>
2022-12-12 23:21:43 -0500

    updating circle

--- R/area_circle.R
+++ R/area_circle.R
@@ -12,18 +12,25 @@
 #' area_circle(7, 0)
 #' area_circle(7, 1)
 area_circle <- function(radius, graph){
+  #checks if the radius is numeric
   if (!is.numeric(radius) | radius < 0){
+    #stops if not
     stop("Circles must have a positive and numeric radius!")
   } else{
+    #calculate the area
     z <- pi*radius^2
+    #checks if graphing is requested
     if(graph == 1){
+      #graphs it and then returns
       print(shapez::graph_circle(radius))
       return(z)
     }
     else if (graph == 0){
+      #retunrs
       return(z)
     }
     else{
+      #stops if the graphing options are not satisfied
       stop('You must input either 0 or 1 to determine graphing options')
     }
   }
