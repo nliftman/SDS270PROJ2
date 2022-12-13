5a35c030e64bf138ed714ffe7f1f782f4cf56f36
nliftman <nliftman@rstudio.smith.edu>
2022-12-12 23:23:34 -0500

    comments to octagon

--- R/area_octagon.R
+++ R/area_octagon.R
@@ -11,19 +11,24 @@
 #' @examples
 #' area_octagon(3, 0 )
 area_octagon <- function(length, graph){
+  #check if length is numeric
   if (!is.numeric(length) | length < 0 ){
     stop("Side lengths of octagons must be positive and numeric!")
   }
   else{
+    #calculate area
     t <- {(2*(1+sqrt(2)))}*length^2
     if (graph == 1){
+      #graph if requested
       print(shapez::graph_octagon(length))
       return(t)
     }
     else if (graph == 0){
+      #return with no graph
       return(t)
     }
     else{
+      #stops if graphing options not met
       stop('You must input either 0 or 1 to determine graphing options')
     }
   }
