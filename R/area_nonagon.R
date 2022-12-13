759bc96e9675e75978a22d3c0122efc8551b7293
nliftman <nliftman@rstudio.smith.edu>
2022-12-12 23:22:57 -0500

    comments to nonagon

--- R/area_nonagon.R
+++ R/area_nonagon.R
@@ -10,10 +10,13 @@
 #' @examples
 #' area_nonagon(5)
 area_nonagon <- function(length){
+  #check if side length is numeric
   if ((!is.numeric(length)) | length < 0 ){
+    #stops if not
     stop('Nonagons must have positive and numeric lengths!', call. = FALSE)
   }
   else{
+    #finds area
     r = ((180/9)*pi)/180
     z <- (9/4)*(length^2)*(cos(r)/sin(r))
     return(z)}
